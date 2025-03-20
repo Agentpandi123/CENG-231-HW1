1105d5d65d89d1add266d01d6ca64a504459beb4
Agentpandi123 <120455155+Agentpandi123@users.noreply.github.com>
2025-03-20 00:25:48 -0700

    Create initials.c

---
+++ initials.c
@@ -0,0 +1,26 @@
+/* initials.c: program should print your initials in large upper-case block letters down the page. Construct each block letter out of the letter it represents. */
+/* Author: Edison Lin */
+
+#include <stdio.h>
+
+/* main function */
+
+int main(void)
+{
+	printf("EEEEEEEEEEEEE\n");
+	printf("E     E     E\n");
+	printf("E     E     E\n");
+	printf("E     E     E\n");
+	printf("E           E\n\n");
+	printf("        YYYYYY\n");
+	printf("       Y\n");
+	printf("YYYYYYY\n");
+	printf("       Y\n");
+	printf("        YYYYYY\n\n");
+	printf("LLLLLLLLLLLLLL\n");
+	printf("L\n");
+	printf("L\n");
+	printf("L\n");
+	printf("L\n");
+	return 0;
+}
